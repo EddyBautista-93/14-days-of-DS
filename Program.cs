6e3e4116b9f05c49f14026bda269a857dc7e30ce
EddyBautista <bautista.j.eddy@gmail.com>
2021-11-03 06:43:59 -0500

    Day 3 question set up

--- Program.cs
+++ Program.cs
@@ -65,7 +65,16 @@ namespace _14_days_of_DS
             }
             return new int[] { 0, 0 };
         }
-    
+
+        //--------------------------------------------------------------
+        // Day 3 - Intersect
+        // Given two integer arrays nums1 and nums2, return an 
+        // array of their intersection.Each element in the 
+        // result must appear as many times as it shows in 
+        // both arrays and you may return the result in any 
+        // order.
+
+
 
     private static void Main(string[] args)
     {
