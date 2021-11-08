b4d41caff68032de768d6f437eba067ed94e1915
EddyBautista <bautista.j.eddy@gmail.com>
2021-11-08 06:44:39 -0600

    q set up for stock q

--- Program.cs
+++ Program.cs
@@ -101,6 +101,21 @@ namespace _14_days_of_DS
             return intersectedList.ToArray();
         }
 
+
+        // Day 4 Best Time to Buy and Sell Stock
+        // You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+        // You want to maximize your profit by choosing a single day to buy one stock and choosing a 
+        // different day in the future to sell that stock.
+
+        // Return the maximum profit you can achieve from this transaction. If you cannot achieve any 
+        // profit, return 0.
+        public int MaxProfit(int[] prices)
+        {
+
+            return 0;
+        }
+
         private static void Main(string[] args)
         {
             // Contains Duplicate
